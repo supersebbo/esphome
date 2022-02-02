bf4108001aeeccfff12741a6dc8d62427142207e
supersebbo <s.d.j.belcher@gmail.com>
2022-02-02 16:25:30 +0000

    Update lcd_rgb.h

--- esphome/components/lcd_rgb/lcd_rgb.h
+++ esphome/components/lcd_rgb/lcd_rgb.h
@@ -8,27 +8,28 @@ const int colorR = 255;
 const int colorG = 0;
 const int colorB = 0;
 
-void setup() 
-{
-    ESP_LOGD("custom", "Setting up RGB Display");
-    // set up the LCD's number of columns and rows:
-    lcd.begin(16, 2);
+class RGBLCD : public Component {
+  public:
+    void setup() override {
+        ESP_LOGD("custom", "Setting up RGB Display");
+        // set up the LCD's number of columns and rows:
+        lcd.begin(16, 2);
 
-    lcd.setRGB(colorR, colorG, colorB);
+        lcd.setRGB(colorR, colorG, colorB);
 
-    // Print a message to the LCD.
-    lcd.print("hello, world!");
+        // Print a message to the LCD.
+        lcd.print("hello, world!");
 
-    delay(1000);
-}
+        delay(1000);
+    }
 
-void loop() 
-{
-    // set the cursor to column 0, line 1
-    // (note: line 1 is the second row, since counting begins with 0):
-    lcd.setCursor(0, 1);
-    // print the number of seconds since reset:
-    lcd.print(millis()/1000);
+    void loop() override {
+        // set the cursor to column 0, line 1
+        // (note: line 1 is the second row, since counting begins with 0):
+        lcd.setCursor(0, 1);
+        // print the number of seconds since reset:
+        lcd.print(millis()/1000);
 
-    delay(100);
-}
+        delay(100);
+    }
+};
